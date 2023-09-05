04cfa0dd0ccda63ac4856c1dcd3c69b423390eee
Ajithvpillai09 <ajithvkallada@gmail.com>
2023-09-06 01:35:05 +0530

    slight chnages

--- frontend/src/User/components/Avatar/Avatar.jsx
+++ frontend/src/User/components/Avatar/Avatar.jsx
@@ -1,12 +1,7 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import {Avatar} from "@nextui-org/react";
-import CameraIcon from "../../components/CameraIcon/CameraIcon";
 import { Button } from "@nextui-org/react";
-import UserNameModal from "../ChangeUserNameModal/UserNameModal";
-import UserEmailModal from "../ChangeEmailModal/ChangeEmailModal";
-import UserNumberModal from "../ChangeNumberModal/ChangeNumberModal";
-import UserPasswordModal from "../ChangePasswordModal/ChangePasswordModal";
 import axios from "axios";
 import { useChangeAvatarMutation,useDeleteAvatarMutation } from "../../slices/api_slices/usersApiSlice";
 import { toast } from "react-toastify";
@@ -60,9 +55,7 @@ const Profile = ()=>{
         }
     }
     return (
-     <section className="h-screen ml-12 pl-5 pr-5 justify-center ">
-        <div className="bg-gray-800 m-12">
-        <div className="max-w-md mx-auto  rounded-xl shadow-md overflow-hidden md:max-w-max mt-12 ">
+        <>
             <div className="md:flex justify-center items-center">
                 <div className="md:shrink-0 relative mt-3">
                 <label className="cursor-pointer">
@@ -83,47 +76,16 @@ const Profile = ()=>{
                 </label>    
                 </div> 
             </div>
-            {!userInfo.isGooleLogin &&
              <div className="flex justify-center">
-                {selectedImage &&
                 <Button color="#01c8ef" onClick={()=>{
                    addProfileImageHandler()
-                }} variant="flat" style={{ color: "#01c8ef" }}>save</Button>
-               }
-                {userInfo?.avatarId &&
+                }} variant="flat" style={{ color: "#01c8ef" }} isLoading={isLoading}>save</Button>
+
                 <Button color="#db2777" onClick={()=>{
                     removeAvatarHandler()
-                }}  variant="flat" style={{ color: "#db2777" }}>delete</Button>
-                 }
-                </div>
-                }
-            <div className="pt-8 flex items-center ">
-                <p className="m-1">User Name:</p>
-                <p className="text-lg font-semibold">{userInfo?.userName}</p>
-                <UserNameModal />
+                }}  variant="flat" style={{ color: "#db2777" }} isLoading={deleteLoading}>delete</Button>
             </div>
-            <div className="flex items-center">
-                <p className="m-1">Email:</p>
-                <p className="text-lg font-semibold">{userInfo?.email}</p>
-                {!userInfo.isGooleLogin &&
-                <UserEmailModal/>
-                }
-            </div>
-            {!userInfo.isGooleLogin &&
-            <>
-            <div className="flex items-center">
-                <p className="m-1">Phone Number:</p>
-                <p className="text-lg font-semibold">{userInfo?.phoneNumber}</p>
-                <UserNumberModal/>
-            </div>
-            <div className="flex items-center justify-center">
-                <UserPasswordModal/>
-            </div>
-            </>
-            }
-        </div>
-    </div>
-    </section>
+        </> 
     )
 }
 
