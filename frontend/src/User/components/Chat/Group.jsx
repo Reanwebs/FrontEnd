f28f1b6551c85ccff3791f54914ac7e18d45ae8d
edwinsiby <edwinsibyrajakumary@gmail.com>
2023-09-25 18:48:32 +0530

    group chat in progress

--- frontend/src/User/components/Chat/Group.jsx
+++ frontend/src/User/components/Chat/Group.jsx
@@ -5,11 +5,11 @@ import { useGetGroupMutation, useCreateGroupChatMutation,useGetGroupChatMutation
 const Group = () => {
   const userInfo = useSelector(state => state.auth.userInfo)
   const userName = userInfo.userName;
-  const socket = new WebSocket(`ws://localhost:5053/ws/group?userName=${userName}`);
   const [groups, setGroups] = useState([])
   const [selectedGroup, setSelectedGroup] = useState(null); 
   const [message, setMessage] = useState(''); 
   const [chatHistory, setChatHistory] = useState([]); 
+  const [socket, setSocket] = useState(null); 
   const [getGroup] = useGetGroupMutation();
   const [createGroupChat] = useCreateGroupChatMutation();
   const [getGroupChat] = useGetGroupChatMutation();
@@ -18,20 +18,25 @@ const Group = () => {
     getGroupHandler()
   },[])
 
-
   const getGroupReq={
-    UserID :userInfo.userName,
+    UserID :userName,
   }
   const getGroupHandler = async ()=>{
     try{
      const groupRes = await getGroup(getGroupReq)
+     console.log(groupRes)
      setGroups(groupRes.data)
     }catch(error){
       console.log(error)
     }
   }
+
+  const createChatReq={
+    UserID  :userName,
+    GroupID :selectedGroup
+  }
   
-  const createGroupChatHandler = async (createChatReq)=>{
+  const createGroupChatHandler = async ()=>{
     try{
       const res = await createGroupChat(createChatReq)
       console.log(res)
@@ -39,10 +44,10 @@ const Group = () => {
       console.log(error)
     }
   } 
-  
-  const getGroupChatHandler = async(getChatReq)=>{
+
+  const getGroupChatHandler = async()=>{
     try{
-     const res = await getGroupChat(getChatReq)
+     const res = await getGroupChat(createChatReq)
       console.log(res)
     }catch(error){
       console.log(error)
@@ -52,7 +57,8 @@ const Group = () => {
   const handleGroupClick = (Group) => {
     createGroupChatHandler()
     getGroupChatHandler()
-    setSelectedGroup(Group);
+    setSelectedGroup(Group.GroupID);
+    connectWebSocket(Group.GroupID);
   };
 
   const handleSendMessage = () => {
@@ -63,25 +69,47 @@ const Group = () => {
       ...prevHistory,
       { Group: selectedGroup, text: message },
     ]);
+    const messageObject = {
+      text: message,
+      sender: userName, 
+      recipient: selectedGroup, 
+    };
+    socket.send(JSON.stringify(messageObject));
     setMessage('');
+  };
+
+  const connectWebSocket = (groupName) => {
+    const ws = new WebSocket(`ws://localhost:5053/ws/group?groupName=${groupName}&userName=${userName}`);
+    ws.onopen = () => {
+      setSocket(ws); 
+      console.log(`WebSocket connection established for group: ${groupName}`);
+    };
+    ws.onclose = () => {
+      console.log(`WebSocket connection closed for group: ${groupName}`);
+    };
+    ws.onerror = (error) => {
+      console.error(`WebSocket error for group: ${groupName}`, error);
+    };
+    ws.onmessage = (event) => {
+      console.log(`Received message for group: ${groupName}`, event.data);
+    };
   };
 
   return (
     <div className="chat-container">
       <div className="users-list">
         <ul>
-          {groups !== null && groups.map((Group) => (
+          {groups !== null && groups.map((Group, index) => (
             <li
-              key={Group.id}
+              key={index}
               onClick={() => handleGroupClick(Group)}
-              className={selectedGroup && selectedGroup.id === Group.id ? 'active' : ''}
+              className={selectedGroup && selectedGroup.id === Group.GroupID ? 'active' : ''}
             >
-              {Group.name}
+              {Group.GroupID}
             </li>
           ))}
         </ul>
       </div>
-
     
       <div className="chat-box">
         {selectedGroup ? (
--- frontend/src/User/components/Chat/Public.jsx
+++ frontend/src/User/components/Chat/Public.jsx
@@ -1,33 +1,47 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect, } from 'react';
+import { useSelector } from 'react-redux';
 const Public = () => {
+  const userInfo = useSelector(state => state.auth.userInfo)
+  const userName = userInfo.userName;
   const [message, setMessage] = useState('');
   const [chatHistory, setChatHistory] = useState([]);
+  const socket = new WebSocket(`ws://localhost:5053/ws/public?userName=${userName}`);
+
+  useEffect(() => {
+    socket.onmessage = (event) => {
+      const receivedMessage = event.data;
+      setChatHistory((prevHistory) => [
+        ...prevHistory,
+        { text: receivedMessage },
+      ]);
+    };
+  }, [socket]);
 
   const handleSendMessage = () => {
     if (message.trim() === '') {
-      return; 
+      return;
     }
-
-   
+    socket.send(message);
     setChatHistory((prevHistory) => [
       ...prevHistory,
-      { text: message },
+      { user:userInfo.userName, text: message },
     ]);
-
     setMessage('');
   };
 
   return (
     <div className="public-chat-container">
       <div className='public-chat-head'>
-           <p>Let's Chat to the world</p>
+        <p>Let's Chat to the world</p>
       </div>
       <div className="public-chat-box">
         {chatHistory.map((message, index) => (
-          <div key={index} className="message">
-            {message.text}
-          </div>
+          <div
+          key={index}
+          className={`message-bubble ${message.user === userInfo.userName ? 'sent-bubble' : 'received-bubble'}`}
+        >
+          {message.text}
+        </div>
         ))}
       </div>
       <div className="public-message-input">
